7e2ca1133783488cafe0284e3204eb15afdab63f
samuolv <samuolvera332@gmail.com>
2026-05-19 18:41:21 -0600

    configura GitHub Actions y Vite base url para despliegue en Pages

--- pagina_web/vite.config.ts
+++ pagina_web/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Analisis_violencia_genero', 
+  base: '/Analisis_violencia_genero/', 
 })
